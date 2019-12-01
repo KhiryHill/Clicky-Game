a4194ff86c999b820a06e0e1574806e03d982dd8
Khiry Hill <khiryhill7@gmail.com>
2019-12-01 14:59:05 -0500

    app.js complete

--- click/src/App.js
+++ click/src/App.js
@@ -1,24 +1,56 @@
-import React, {Component}from 'react';
+import React, { Component } from 'react';
 import './App.css';
-// import characters from "./characters.json";
-import Title from "./component/Title";
+import characters from "./characters.json";
+// import Title from "./component/Title";
 import Wrapper from "./component/Wrapper"
-import CharacterCard from "./component/CharacterCard"
+import Card from "./component/Card"
 
 class App extends Component {
   state = {
-    CharacterCard
+    characters,
+    score: 0,
+    highscore: 0,
+    chosenArray: [],
+    alertMessage: "",
+    shakeit: false
   };
+  clickPic = id => {
+    // this will rearrange card randomly
+    const shuffledArray = this.shuffledArray(characters);
+    this.setState({ characters: shuffledArray });
+    if (this.state.chosenArray.includes(id)) {
+      this.setState({ score: 0, chosenArray: [], alertMessage: "Incorrect Choice. Try Again.", shakeit: true })
+    }
+    else {
+      this.setState({
+        chosenArray: this.state.chosenArray.push([id]),
+        score: this.state.score + 1,
+        alertMessage: "Correct!",
+        shakeit: false
+      });
+    }
+    if(this.state.score > this.state.highscore){
+      this.setState({highscore: this.state.score})
+      console.log({highscore: this.state.score})
+    }
+  }
+  shuffledArray = (picArray) =>{
+    for (let i= picArray.length -1; i > 0; i--){
+      const k = Math.floor(Math.random()* (i+1));
+    [picArray[i], picArray[k]] = [picArray[k], picArray[i]]
+    }
+    return picArray;
+  }
 
   render() {
     return (
       <Wrapper>
-        <Title>Clicky Game</Title>
-        {/* {this.state.CharacterCard.map(CharacterCard => ( */}
-          <CharacterCard
-            // id={characters.id}
-            // name={characters.name}
-            // image={this.state.CharacterCard.image}
+        {this.state.characters.map(characters => (
+          <Card
+            id={characters.id}
+            key={characters.id}
+            name={characters.name}
+            image={characters.image}
           />
         ))}
       </Wrapper>
--- click/src/component/Card/index.js
+++ click/src/component/Card/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function CharacterCard(props) {
     return (
-        <div className="card" onClick={() => props.clickCount(props.id)}>
+        <div className="card" onClick={() => props.clickPic(props.id)}>
             <div className="img-container">
                 <img alt={props.name} src={props.image} />
             </div>
