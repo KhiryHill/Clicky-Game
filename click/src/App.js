0d65b24d6dd9df95cc3542f55c3aa704f26ac3a1
Khiry Hill <khiryhill7@gmail.com>
2019-12-01 13:45:12 -0500

    added charcter json

--- click/src/App.js
+++ click/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}from 'react';
 import './App.css';
+// import characters from "./characters.json";
+import Title from "./component/Title";
+import Wrapper from "./component/Wrapper"
+import CharacterCard from "./component/CharacterCard"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    CharacterCard
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Clicky Game</Title>
+        {/* {this.state.CharacterCard.map(CharacterCard => ( */}
+          <CharacterCard
+            // id={characters.id}
+            // name={characters.name}
+            // image={this.state.CharacterCard.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+};
 
 export default App;
