959f1d962d6ce460f408e6d454989cc902ec39db
Khiry Hill <khiryhill7@gmail.com>
2019-12-09 18:52:23 -0500

    Updates

---
+++ precache-manifest.644a9433e9bbec6f624492fcf83b3a54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c629d1355449a3ef5070308897aa0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61cc6cb45a39e82c7a2d",
+    "url": "/static/css/main.e8d0f34d.chunk.css"
+  },
+  {
+    "revision": "f43b7c77d821edb14504",
+    "url": "/static/js/2.99929d89.chunk.js"
+  },
+  {
+    "revision": "61cc6cb45a39e82c7a2d",
+    "url": "/static/js/main.d1cce4d5.chunk.js"
+  },
+  {
+    "revision": "201df16e0dc70e9cb02b",
+    "url": "/static/js/runtime-main.44fa52dd.js"
+  }
+]);
